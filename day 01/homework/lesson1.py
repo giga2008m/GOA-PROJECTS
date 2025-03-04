d4566efa18c23fff94d7266ab424659a03a47f79
giga2008m <gigamena21@gmail.com>
2025-03-04 23:15:25 +0400

    first commit

---
+++ day 01/homework/lesson1.py
@@ -0,0 +1,5 @@
+name=("giga")
+surname=("menabdishvili")
+age=16
+height=1.70
+print("I am"+" "+name+" "+surname+" "+str(age)+ " "+"years old"+" "+str(height)+"m"+" "+"height")
---
+++ day 03/class work/classwork.py
@@ -0,0 +1,5 @@
+#user sign in website
+firstname=input("enter your name:")
+lastname=input("enter your lastname:")
+agestr=input("your age:")
+password=input("enter password:")
